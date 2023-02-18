0563d61f53bc644d930f5a4540d45b86453fd9d8
Preet Sojitra <preet.dev373@gmail.com>
2023-02-19 05:01:15 +0530

    feat: integrate flask api with ml model

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
-import Profile from "./Pages/Profile";
-import Home from "./Pages/Home";
+import Profile from "./Pages/Profile"
+import Home from "./Pages/Home"
+import Researchers from "./Pages/Researchers"
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/profile/:profileId" element={<Profile/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:profileId" element={<Profile />} />
+          <Route path="/researchers" element={<Researchers />} />
         </Routes>
       </BrowserRouter>
     </>
