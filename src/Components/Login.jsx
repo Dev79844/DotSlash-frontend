ad9dc14d2dea6eb70f11237b77a1ea165ae8d0f0
Preet Sojitra <preet.dev373@gmail.com>
2023-02-18 17:34:36 +0530

    add: login

---
+++ src/Components/Login.jsx
@@ -0,0 +1,106 @@
+import React from "react"
+import { Icon } from "@iconify/react"
+import Modal from "react-modal"
+import SignUp from "./SignUp"
+
+export default function Login(props) {
+
+     const [modalIsOpen, setModalIsOpen] = React.useState(false)
+
+     const customStyles = {
+       overlay: {
+         backgroundColor: "rgba(0, 0, 0, 0.8)",
+       },
+       content: {
+         height: "fit-content",
+         top: "50%",
+         left: "50%",
+         transform: "translate(-50%, -50%)",
+         padding: 0,
+         width: "50vw",
+       },
+     }
+
+     function openModal() {
+       setModalIsOpen(true)
+     }
+
+     function closeModal() {
+       setModalIsOpen(false)
+     }
+
+  const [loginData, setLoginData] = React.useState({
+    email: "",
+    password: "",
+  })
+
+  const handleLoginData = (e) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    })
+    }
+
+    return (
+      <>
+        <div className="font-Roboto">
+          <div className="relative ">
+            <Icon
+              icon="radix-icons:cross-2"
+              color="#f03a47"
+              className="text-3xl cursor-pointer absolute right-0 lg:right-6 lg:top-5"
+              onClick={props.closeModal}
+            />
+
+            <div className="pt-12 mx-4 pb-8 lg:mx-6 lg:pt-20 lg:pb-16">
+              <div className="sm:flex sm:gap-8 sm:justify-between ">
+                <input
+                  type="email"
+                  placeholder="Enter email"
+                  className="border-b border-black px-2 py-1 text-xl w-full  placeholder:text-black placeholder:font-bold"
+                  value={loginData.email}
+                  name="email"
+                  onChange={handleLoginData}
+                />
+              </div>
+
+              <input
+                type="password"
+                placeholder="Enter password"
+                className="border-b border-black px-2 py-1 mt-3 text-xl w-full placeholder:text-black placeholder:font-bold"
+                name="password"
+                value={loginData.password}
+                onChange={handleLoginData}
+              />
+
+              <div className="flex justify-between mt-2">
+                <h1>Not yet registered? </h1>
+                <h1 className="cursor-pointer" onClick={openModal}>
+                  Register Here
+                </h1>
+              </div>
+              <button
+                type="submit"
+                className="block w-3/5 mt-4 mx-auto font-medium border-secondary border-2 hover:bg-secondary hover:text-white transition-colors duration-500 px-4 text-lg rounded py-1 sm:w-2/5"
+                onClick={() => {
+                  console.log(loginData)
+                }}
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Register Modal */}
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          ariaHideApp={false}
+        >
+          <SignUp closeModal={closeModal} />
+        </Modal>
+      </>
+    )
+}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React from "react"
+import Modal from "react-modal"
+import Login from "./Login"
 
 export default function Navbar() {
-    return (
-        <div>
-            <h1>LOGO</h1>
-            <h1>Login</h1>
-        </div>
-    )
+  const [modalIsOpen, setModalIsOpen] = React.useState(false)
+
+ const customStyles = {
+   overlay: {
+     backgroundColor: "rgba(0, 0, 0, 0.8)",
+   },
+   content: {
+     height: "fit-content",
+     top: "50%",
+     left: "50%",
+     transform: "translate(-50%, -50%)",
+     padding: 0,
+     width: "50vw",
+   },
+ }
+
+  function openModal() {
+    setModalIsOpen(true)
+  }
+
+  function closeModal() {
+    setModalIsOpen(false)
+  }
+
+  return (
+    <>
+      <div className="flex justify-between px-16 py-1 text-2xl">
+        <h1>LOGO</h1>
+        <h1 className="cursor-pointer" onClick={openModal}>
+          Login
+        </h1>
+      </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        ariaHideApp={false}
+      >
+        <Login closeModal={closeModal} />
+      </Modal>
+    </>
+  )
 }
